c721dfcd5fc513aaab6d7da34e274b711dc58abf
Benjamin Rowe <benjamin.alan.rowe@gmail.com>
2020-10-15 15:27:58 +1000

    fixed initfunction not being called

--- src/ShadedImage.js
+++ src/ShadedImage.js
@@ -1,47 +1,52 @@
-import { useRef, useState, useEffect } from "react";
-import React from "react";
+import { useRef, useState, useEffect } from 'react'
+import React from 'react'
 import {
   compileShader,
   createProgram,
   initBuffers,
   bindBuffers,
-  loadTexture,
-} from "./shaderUtils";
+  loadTexture
+} from './shaderUtils'
 
 export default function ShadedImage({ style, className, shaders, image }) {
-  const canvasRef = useRef();
+  const canvasRef = useRef()
 
   useEffect(() => {
-    let canvas = canvasRef.current;
-    console.log(canvas);
+    let canvas = canvasRef.current
+    console.log(canvas)
     // Initialize our gl context
-    let gl = canvas.getContext("webgl");
-    init(canvas, gl);
-    console.log(shaders);
-    let compiledShaders = createShaders(gl, shaders);
-    let texture = loadTexture(gl, image);
+    let gl = canvas.getContext('webgl')
+    init(canvas, gl)
+    console.log(shaders)
+    let compiledShaders = createShaders(gl, shaders)
 
-    const fb = gl.createFramebuffer();
+    for (let shader of compiledShaders) {
+      shader.initFunction({ program: currentProgram.program })
+    }
+
+    let texture = loadTexture(gl, image)
+
+    const fb = gl.createFramebuffer()
 
     // attach the texture as the first color attachment
-    const attachmentPoint = gl.COLOR_ATTACHMENT0;
+    const attachmentPoint = gl.COLOR_ATTACHMENT0
     gl.framebufferTexture2D(
       gl.FRAMEBUFFER,
       attachmentPoint,
       gl.TEXTURE_2D,
       texture.texture,
       0
-    );
+    )
 
-    let buffers = initBuffers(gl);
-    animate(canvas, gl, compiledShaders, texture, buffers);
-  }, [canvasRef, shaders, image]);
+    let buffers = initBuffers(gl)
+    animate(canvas, gl, compiledShaders, texture, buffers)
+  }, [canvasRef, shaders, image])
 
   return (
     <div>
       <canvas ref={canvasRef} {...{ style, className }} />
     </div>
-  );
+  )
 }
 
 function init(canvas, gl) {
@@ -58,17 +63,17 @@ function init(canvas, gl) {
         window.oRequestAnimationFrame ||
         window.msRequestAnimationFrame ||
         function (callback, element) {
-          window.setTimeout(callback, 1000 / 60);
+          window.setTimeout(callback, 1000 / 60)
         }
-      );
-    })();
+      )
+    })()
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.enable(gl.DEPTH_TEST);
-  gl.depthFunc(gl.LESS);
-  gl.enable(gl.BLEND);
-  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-  return gl;
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LESS)
+  gl.enable(gl.BLEND)
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+  return gl
 }
 
 function resizeCanvas(canvas, gl) {
@@ -76,61 +81,61 @@ function resizeCanvas(canvas, gl) {
     canvas.width !== canvas.clientWidth ||
     canvas.height !== canvas.clientHeight
   ) {
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
+    canvas.width = canvas.clientWidth
+    canvas.height = canvas.clientHeight
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.viewport(0, 0, canvas.width, canvas.height)
   }
 }
 
 function createShaders(gl, shaders) {
   let compiledShaders = shaders.map((shader) => {
-    console.log(shader.vertexShader);
-    let vertexShader = compileShader(gl, shader.vertexShader, gl.VERTEX_SHADER);
+    console.log(shader.vertexShader)
+    let vertexShader = compileShader(gl, shader.vertexShader, gl.VERTEX_SHADER)
     let fragmentShader = compileShader(
       gl,
       shader.fragmentShader,
       gl.FRAGMENT_SHADER
-    );
+    )
 
-    let shaderProgram = createProgram(gl, vertexShader, fragmentShader);
+    let shaderProgram = createProgram(gl, vertexShader, fragmentShader)
 
     return {
       program: shaderProgram,
       initFunction: shader.initFunction,
-      updateFunction: shader.updateFunction,
-    };
-  });
-  return compiledShaders;
+      updateFunction: shader.updateFunction
+    }
+  })
+  return compiledShaders
 }
 
 function animate(canvas, gl, shaders, texture, buffers) {
-  resizeCanvas(canvas, gl);
-  render(gl, shaders, texture, buffers);
+  resizeCanvas(canvas, gl)
+  render(gl, shaders, texture, buffers)
   window.requestAnimationFrame(() =>
     animate(canvas, gl, shaders, texture, buffers)
-  );
+  )
 }
 
 function render(gl, shaders, texture, buffers) {
-  gl.bindTexture(gl.TEXTURE_2D, texture.texture);
+  gl.bindTexture(gl.TEXTURE_2D, texture.texture)
 
   for (let [i, currentProgram] of shaders.entries()) {
-    if (!currentProgram) return;
+    if (!currentProgram) return
 
     // // Load program into GPU
-    gl.useProgram(currentProgram.program);
-    currentProgram.updateFunction({ program: currentProgram.program });
+    gl.useProgram(currentProgram.program)
+    currentProgram.updateFunction({ program: currentProgram.program })
 
     // Bind buffers to the current program
-    bindBuffers(gl, currentProgram.program, buffers);
+    bindBuffers(gl, currentProgram.program, buffers)
 
     if (i == shaders.length - 1) {
       // render to the canvas
-      gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+      gl.bindFramebuffer(gl.FRAMEBUFFER, null)
     }
 
     // Draw the data from our buffers
-    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0)
   }
 }
